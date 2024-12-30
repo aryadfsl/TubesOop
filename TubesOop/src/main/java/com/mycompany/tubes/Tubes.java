5b07f29bdde03857edaab952e976e634a1792bc7
Arya <muhamad.arya@mhs.itenas.ac.id>
2024-12-30 12:06:10 +0700

    “TubesOop”

---
+++ TubesOop/src/main/java/com/mycompany/tubes/Tubes.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tubes;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Tubes {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
